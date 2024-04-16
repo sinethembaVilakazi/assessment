8c30d5e430a559538c01839c36daeceb6df37d22
sivilak022 <sivilak022@student.wethinkcode.co.za>
2024-04-16 20:39:45 +0200

    initial commit

---
+++ src/main/java/com/enviro/assessment/grad001/SinethembaVilakazimain/SinethembaVilakaziMainApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.SinethembaVilakazimain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinethembaVilakaziMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinethembaVilakaziMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/SinethembaVilakazimain/SinethembaVilakaziMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.SinethembaVilakazimain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinethembaVilakaziMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
